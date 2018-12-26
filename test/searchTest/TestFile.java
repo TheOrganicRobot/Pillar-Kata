4edab8b319567c20e8e313b8763c4917f46e31c1
Derek Ball <dgwb37@gmail.com>
2018-12-26 12:17:21 -0500

    Refactor tests for clarity

--- test/searchTest/TestFile.java
+++ test/searchTest/TestFile.java
@@ -4,32 +4,33 @@ import static org.junit.Assert.*;
 
 import java.util.List;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import searchKata.FileRead;
 import searchKata.GetWords;
 
 public class TestFile {
-
 	
+	private String filePath;
+	private FileRead file;
+	private List<String[]> fileReadOutput;
+	
+	@Before
+	public void setup() {
+		filePath = "StarTrekSearch.csv";
+		file = new FileRead();
+		fileReadOutput = file.fileReader(filePath);
+	}
 	@Test
 	public void testFileRead() {
-		
-		String filePath = "StarTrekSearch.csv";
-		FileRead file = new FileRead();
-		List<String[]> fileReadOutput = file.fileReader(filePath);
 		assertEquals(16, fileReadOutput.size());
 		
 	}
 	@Test
-	public void testGetWords() {
-		
+	public void testGetWords() {	
 		GetWords wordsToSearch = new GetWords();
-		String filePath = "StarTrekSearch.csv";
-		FileRead file = new FileRead();
-		List<String[]> fileReadOutput = file.fileReader(filePath);
-		List<String> myWords = wordsToSearch.getWordsToSearch(fileReadOutput);
-		
+		List<String> myWords = wordsToSearch.getWordsToSearch(fileReadOutput);	
 		assertEquals(7, myWords.size());
 	}
 
