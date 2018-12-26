97bbe265d03d94af00156b5e6dafe476b9a62712
Derek Ball <dgwb37@gmail.com>
2018-12-26 10:55:39 -0500

    first commit

---
+++ src/searchKata/FileRead.java
@@ -0,0 +1,5 @@
+package searchKata;
+
+public class FileRead {
+
+}
---
+++ src/searchKata/WordSearch.java
@@ -0,0 +1,10 @@
+package searchKata;
+
+public class WordSearch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
